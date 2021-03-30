ea5b93adb9a719393d52908c1cdd52c770111fb2
Sabrina Kuah <swkuah@gmail.com>
2021-03-30 12:30:44 -0700

    Add logout functionality, closes #18

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,35 +3,37 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import base from '../styles/styles';
 import { FontAwesome } from '@expo/vector-icons';
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import UserModal from '../components/UserModal';
+import firebase from 'firebase';
 
-const HomeScreen = ({ route }) => {
+const HomeScreen = ({ route, navigation }) => {
   const user = route.params;
   const [modalVisible, setModalVisible] = useState(false);
 
+  const handleLogOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log('Logout successful');
+        navigation.navigate('LoginScreen');
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error>>', errorMessage);
+      });
+  };
+
   return (
     <SafeAreaView style={base.container}>
-      <Modal
-        animationType='slide'
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text style={styles.textStyle}>Close</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+      <UserModal
+        setModalVisible={setModalVisible}
+        modalVisible={modalVisible}
+        handleLogOut={handleLogOut}
+      />
       <View>
         <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
           <FontAwesome name='user' color='white' size={20} />
@@ -42,48 +44,6 @@ const HomeScreen = ({ route }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default HomeScreen;
