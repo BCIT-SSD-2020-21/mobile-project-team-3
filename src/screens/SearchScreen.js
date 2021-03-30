2b34bcbf77d4697e6f78acbb8dbf3a3d1c4a1f43
mandeep1580 <mandeep15@gmail.com>
2021-03-30 05:27:16 -0700

    added baseurl for api and working on get method for renderign the stocks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, {useState}from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons';
 import SearchBar from '../components/SearchBar';
+import finnhub from '../api/finnhub'
 
 const SearchScreen = () => {
   const [stock, setStock] = useState('')
@@ -9,9 +10,20 @@ const SearchScreen = () => {
     <View>
         <SearchBar
         stock={stock}
-        onStockChange={(newStock) => setStock(newStock)}/>
+        onStockChange={(newStock) => setStock(newStock)}
+        onStockSubmit={() => searchApi()}/>
     </View>
   );
+}
+
+//api calls
+const searchApi = async () => {
+  const response = await finnhub.get('/webhook', {
+      params: {
+        'event': 'earnings', 
+        'symbol': 'AAPL'
+      }
+  });
 }
 
 const styles = StyleSheet.create({});
