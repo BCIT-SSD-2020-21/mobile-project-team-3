fc2c00d453aac8923a36bf3e6972f096b84d4e74
Sabrina Kuah <swkuah@gmail.com>
2021-03-30 12:16:03 -0700

    Add user modal, #18

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,89 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import base from '../styles/styles';
+import { FontAwesome } from '@expo/vector-icons';
+import { Alert, Modal, StyleSheet, Text, Pressable, View } from 'react-native';
 
 const HomeScreen = ({ route }) => {
   const user = route.params;
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaView style={base.container}>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <View>
+        <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+          <FontAwesome name='user' color='white' size={20} />
+        </TouchableOpacity>
+      </View>
       <Text style={{ color: 'white' }}>Welcome, {user.email}! </Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default HomeScreen;
